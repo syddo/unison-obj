cf289f32b9d27b05f54f665287a595ed4d9d9f24
Syd Pao <14306322+syddo@users.noreply.github.com>
2022-06-14 23:39:49 +0800

    init project

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! Unison Object definitions for Test Program Sources
+//!
+//! # Library structure
+//!
+//! This library contains definitions for every Unison Object.
+//!
+//! The information for each object is separated into submodules, named after
+//! the object name itself.
+
+pub mod applicationlibrary;
